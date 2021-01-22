6ee4fbb8d448214aee5c9ad53a91d0dfa6a9e3ba
saifulalamfahim <fahim12345saiful@gmail.comm>
2021-01-22 18:05:51 +0600

    last change

---
+++ assignment.js
@@ -0,0 +1,86 @@
+//start kilometerToMeter function..
+
+function kilometerToMeter(kilometer) {
+    if(kilometer > 0){
+      var meter = kilometer * 1000; // 1 kilometer = 1000 meter..
+      return meter;
+    }
+    else{
+      return  "kilometer is not availale on negative";
+    }
+}
+var motherGaveMeKilometer = kilometerToMeter(-0.45);
+console.log(motherGaveMeKilometer);
+var fatherGaveMeKilometer = kilometerToMeter(7);
+console.log(fatherGaveMeKilometer);
+
+//end kilometerToMeter function..
+
+
+//start budgetCalculater function..
+
+function budgetCalculater(watch, mobile, laptop){
+  var budget1 = watch * 50;   // perWatchPrice = 50;
+  var budget2 = mobile * 100; // perMobilePrice = 100;
+  var budget3 = laptop * 500; // perLaptopPrice = 500;
+  var totalBudget = budget1 + budget2 + budget3;
+  return totalBudget;
+}
+var gadgetQuantity = budgetCalculater(8, 7, 9);
+console.log(gadgetQuantity);
+
+//end budgetCalculater function..
+
+
+//start hotelCost function..
+
+function hotelCost(day) {
+  if(day > 0){
+    var money = 0;
+    if(day <= 10) {
+      money = day * 100; // per day holtel cost of first 1-10 days = 100tk..
+    }
+    else if(day <= 20) {
+      var firstTenDay = 10 * 100;
+      var remaining = day - 10;
+      var secondTenDay = remaining * 80; // per day holtel cost of second 11-20 days = 80tk..
+      money = firstTenDay + secondTenDay;
+    }
+    else {
+      var firstTenDay = 10 * 100;
+      var secondTenDay = 10 * 80;
+      var remaining = day - 20;
+      var anotherDay = remaining * 50; // per day holtel cost of after 20 days = 50tk..
+      money = firstTenDay + secondTenDay + anotherDay;
+    }
+    return money;
+  }
+  else{
+    return  "day is not availale on negative";
+  }
+}
+var totalCost = hotelCost(-0.45);
+console.log(totalCost);
+
+//end hotelCost function..
+
+
+//start megaFriend function..
+
+function megaFriend(name) {
+  var biggestName = "";
+  var biggerFriend;
+
+  for (var i = 0; i < name.length; i++) {
+    if (name[i].length > biggestName) {
+      var biggestName = name[i].length;
+      biggerFriend = name[i];
+    }
+  }
+    return biggerFriend;
+}
+var friendName = megaFriend(["hamara","ninja","haturi", "jiyan", "doremon", "per", "man", "shishimanu", "dora"]);
+console.log(friendName);
+
+//end megaFriend function..
+
